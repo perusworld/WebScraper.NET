0545fe31b179a79c9c424e8f69356201222fb76a
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2016-09-27 18:10:14 -0700

    Release 2.0.2.0

--- WebScraper.NET/Properties/AssemblyInfo.cs
+++ WebScraper.NET/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebScraper.NET")]
-[assembly: AssemblyDescription(".NET based Web Scraper Framework")]
+[assembly: AssemblyDescription("A .Net based Web Scraper using the WebBrowser control")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebScraper.NET")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.1")]
+[assembly: AssemblyVersion("2.0.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
